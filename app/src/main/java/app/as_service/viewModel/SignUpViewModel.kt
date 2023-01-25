7e19e49c3dc14713698e72c4f99d4f70b7b703a8
LeeJY <52855326+tekken5953@users.noreply.github.com>
2023-01-25 13:58:33 +0900

    Revert "revert"

--- app/src/main/java/app/as_service/viewModel/SignUpViewModel.kt
+++ app/src/main/java/app/as_service/viewModel/SignUpViewModel.kt
@@ -1,18 +1,29 @@
 package app.as_service.viewModel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import app.as_service.dao.StaticDataObject.signUpRepository
+import app.as_service.dao.StaticDataObject.TAG
+import app.as_service.repository.SignUpRepo
 
 class SignUpViewModel : ViewModel() {
-    private val signUpResultData: LiveData<String>
-        get() = signUpRepository._signUpResultData
+    // MutableLiveData 값을 받아 View 로 전달해 줄 LiveData
+    private lateinit var signUpResultData: LiveData<String>
+    private val repo = SignUpRepo()
 
-    fun loadSignUpResult() {
-        signUpRepository.loadSignUpResult()
+    // MutableLiveData 값을 갱신하기 위한 함수
+    fun loadSignUpResult(username: String,phone: String,password: String) {
+        repo.loadSignUpResult(username,phone, password)
     }
 
+    // LiveData 에 MutableLiveData 값 적용 후 View 에 전달
     fun getSignUpCode() : LiveData<String> {
+        signUpResultData = repo._signUpResultData
         return signUpResultData
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d(TAG, "회원가입 뷰모델 인스턴스 소멸")
     }
 }
