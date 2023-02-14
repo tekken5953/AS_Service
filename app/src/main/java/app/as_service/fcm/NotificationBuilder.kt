4a2f3f021621a4d4aa8e9ba46e5d3ba39804e9c9
JaeYoung <tekken5953@naver.com>
2023-02-14 18:00:06 +0900

    _<h1>2023-02-09 이재영 Commit - WIP </h1>_
    * 리스트 화면의 텍스트의 길이제한을 설정함.
     - 최대글자 설정
     - 넘으면 ...으로 표시
    
    * 공공날씨 데이터 가공
     - UI 변경
     - 버그픽스
    
    * Target SDK 버전을 33으로 올림
     - 알림권한 요청을 따로 해주어야 해서 메인엑티비티 + 매니페스트에 추가
    
    * Firebase FCM 사용을 위한 셋팅
     - 프로젝트 생성 후 서버에 json 파일 전송
     - 디바이스 targetToken 받아오기
     - 서버에서 안드로이드로 FCM 메시지 발송
     - 안드로이드에서 메시지 수신 > Notification 생성 후 발생
     - 헤드업 노티피케이션으로 변경
    
    * 레이아웃 구성 변경
     - 기존 ViewPager2(Fragment) + Activity 에서
     - Fragment + Activity 로 변경
     - 스와이프의 불필요성과 갱신주기의 단축 때문.
    
    * 코리아 빌드위크 전시를위해 admin 계정 접속가능
     - 더미데이터로 as-eye만 한개 노출
     - admin/admin으로 접속

--- app/src/main/java/app/as_service/fcm/NotificationBuilder.kt
+++ app/src/main/java/app/as_service/fcm/NotificationBuilder.kt
@@ -1,69 +1,50 @@
 package app.as_service.fcm
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.res.Resources
-import android.graphics.BitmapFactory
-import android.media.RingtoneManager
-import android.os.Build
+import android.graphics.Color
 import androidx.core.app.NotificationCompat
 import app.as_service.R
-import app.as_service.view.login.LoginActivity
 import com.google.firebase.messaging.RemoteMessage
 
 
-class NotificationBuilder(context: Context, data: RemoteMessage,title : String) : BroadcastReceiver() {
-//    var INTENT_ACTION = Intent.ACTION_BOOT_COMPLETED
-    private val mData = data
-    private val mContext = context
-    private var mTitle = title
-
-    fun getNotification() {
-        val intent = Intent(mContext, NotificationBuilder::class.java)
-        onReceive(mContext, intent)
-    }
-
-    override fun onReceive(context: Context, intent: Intent?) {
+class NotificationBuilder {
+    // foreground 상태에서 해드업 알림
+     fun sendNotification2(context: Context, intent: Intent, data: RemoteMessage, title : String) {
         val notificationManager =
-            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-//        val manager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val intentActivity = Intent(context, LoginActivity::class.java)
-        intentActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        val pendingIntent = PendingIntent.getActivity(
-            context,
-            1,
-            intentActivity,
-            PendingIntent.FLAG_IMMUTABLE
-        )
-        val builder = NotificationCompat.Builder(context, "default")
-        builder.setContentText(mData.notification!!.body)
-        builder.setSmallIcon(R.drawable.app_icon)
-        val large = BitmapFactory.decodeResource(Resources.getSystem(), R.drawable.star_fill)
-        builder.setLargeIcon(large)
-        builder.setContentTitle(mTitle)
-        builder.setWhen(System.currentTimeMillis())
-        builder.setBadgeIconType(NotificationCompat.BADGE_ICON_LARGE)
-        val ringtoneUri = RingtoneManager.getActualDefaultRingtoneUri(
-            context,
-            RingtoneManager.TYPE_NOTIFICATION
-        )
-        builder.setSound(ringtoneUri)
-        val vibrate = longArrayOf(0, 100, 200, 300)
-        builder.setVibrate(vibrate)
-        builder.setContentIntent(pendingIntent)
-        builder.setAutoCancel(true)
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
-            notificationManager.createNotificationChannel(
-                NotificationChannel(
-                    "default", "Channel",
-                    NotificationManager.IMPORTANCE_DEFAULT
-                )
-            )
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+        val notificationChannelId = "500"
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        val notificationChannel = NotificationChannel(
+            notificationChannelId,
+            "푸시 알림",
+            NotificationManager.IMPORTANCE_HIGH
+        ).apply {
+            description = "Channel description"
+            enableLights(true)
+            lightColor = Color.RED
+            vibrationPattern = longArrayOf(0, 100, 200, 300)
+
+            enableVibration(true)
         }
-        notificationManager.notify(1, builder.build())
+
+        notificationManager!!.createNotificationChannel(notificationChannel)
+
+        val notificationBuilder = NotificationCompat.Builder(context, notificationChannelId)
+        notificationBuilder.setAutoCancel(true)
+            .setDefaults(Notification.DEFAULT_ALL)
+            .setWhen(System.currentTimeMillis())
+            .setSmallIcon(R.drawable.app_icon)
+            .setTicker("TICKER")
+            .setPriority(NotificationManager.IMPORTANCE_HIGH)
+            .setContentIntent(pendingIntent)
+            .setContentTitle(title)
+            .setContentText(data.data.toString())
+        //.setContentInfo("Info");
+        notificationManager.notify( /*notification id*/1, notificationBuilder.build())
     }
 }
